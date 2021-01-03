b4f03ff257cafac9b7a5915251e24cbb1f6fe461
APPS4444 <aparnajitripathi@gmail.com>
2021-01-03 23:40:11 +0530

    Updates

---
+++ precache-manifest.ee453b4431dd76ebab729df16e7f1acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e35d2d70713adf54c884673c331830",
+    "url": "/React-Todo-app/index.html"
+  },
+  {
+    "revision": "a307579d68c03a74395d",
+    "url": "/React-Todo-app/static/css/main.b2964f3f.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/React-Todo-app/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Todo-app/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a307579d68c03a74395d",
+    "url": "/React-Todo-app/static/js/main.796e82ae.chunk.js"
+  },
+  {
+    "revision": "938764b8b305770ebc20",
+    "url": "/React-Todo-app/static/js/runtime-main.4b451c28.js"
+  }
+]);
